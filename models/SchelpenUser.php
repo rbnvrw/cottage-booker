662446c1dba1881bd87d08de77d03713ceb1f865
Ruben Verweij <development@rubenverweij.nl>
2016-03-17 18:13:59 +0100

    First refactor

--- models/SchelpenUser.php
+++ models/SchelpenUser.php
@@ -1,22 +1,29 @@
 <?php
 
-class SchelpenUser {
-	
-	public function __construct(){
-		
-	}
-	
-	public function setupUser($ui){
-		$oDb = Loader::db();
-        
+/**
+ * Model for user who can book the cottage
+ */
+class SchelpenUser
+{
+
+    /**
+     * Fetch credits for this user
+     * @param $ui
+     */
+    public function setupUser($ui)
+    {
+        $oDb = Loader::db();
+
         // Fetch general settings
-        $sSql = "SELECT * FROM btCottageBooker WHERE userCreditsMax > 0 AND userCreditsAnnual > 0 LIMIT 1";
+        $sSql = "SELECT * FROM btCottageBooker WHERE userCreditsMax > 0"
+                . "AND userCreditsAnnual > 0 LIMIT 1";
         $aConfig = $oDb->getRow($sSql);
-		
-		$iCredits = ($aConfig['userCreditsAnnual'] > 0) ? intval($aConfig['userCreditsAnnual']) : 42;
-	
-		$ui->setAttribute('cottage_booker_credits', $iCredits);
-		$ui->setAttribute('cottage_booker_credits_last_update', date('Y-m-d'));
-	}
-	
+
+        $iUserCredits = $aConfig['userCreditsAnnual'];
+        $iCredits = ($iUserCredits > 0) ? intval($iUserCredits) : 42;
+
+        $ui->setAttribute('cottage_booker_credits', $iCredits);
+        $ui->setAttribute('cottage_booker_credits_last_update', date('Y-m-d'));
+    }
+
 }
