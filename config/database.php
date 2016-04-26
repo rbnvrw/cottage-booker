bdd437d78dcf8e77ade14cb62bdcce503436274e
Ruben Verweij <development@rubenverweij.nl>
2016-04-26 22:43:44 +0200

    Add seeders, models for Booking table

--- config/database.php
+++ config/database.php
@@ -46,12 +46,6 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
@@ -64,19 +58,7 @@ return [
             'prefix' => '',
             'strict' => false,
             'engine' => null,
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
+        ]
 
     ],
 
@@ -91,30 +73,6 @@ return [
     |
     */
 
-    'migrations' => 'migrations',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
-
-    'redis' => [
-
-        'cluster' => false,
-
-        'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-        ],
-
-    ],
+    'migrations' => 'migrations'
 
 ];
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,14 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Booking::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => $faker->userId,
+        'start' => rand(time(), time()+1209600),
+        'end' => rand(time()+1209600, time()+2419200),
+        'costs' => rand(5, 25),
+        'comments' => str_random(150),
+        'persons' => rand(1, 15)
+    ];
+});
